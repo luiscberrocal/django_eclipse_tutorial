b5b2fac6657c97c9366b77b403073c44ccee2a01
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2012-07-16 08:18:22 -0500

    Added the templates from the Django distribution.

--- lm_stats_site/settings.py
+++ lm_stats_site/settings.py
@@ -106,6 +106,7 @@ ROOT_URLCONF = 'lm_stats_site.urls'
 WSGI_APPLICATION = 'lm_stats_site.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/Users/luiscberrocal/Documents/ws_38/lm_stats_site/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
